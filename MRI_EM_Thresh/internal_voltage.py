aa08ed139c509b8a200206a8ee4d52f65b6c1e43
luc-lgtm <lucienlearyadams@gmail.com>
2025-10-27 11:52:32 -0400

    first_edit

---
+++ MRI_EM_Thresh/internal_voltage.py
@@ -0,0 +1,5 @@
+# Calculator for internal voltage given B_ROC, B_MAX, outputting internal
+# voltage and whether or not its afe given biological constraints. Also measures
+# how B field from MRI interacts with brain's EM
+
+print("test")
